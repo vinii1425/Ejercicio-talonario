ddc23d9eb7d1a246f07909d11cfd446bddcec793
Rodrigo Hernandez <vinii1425@gmail.com>
2026-03-07 11:05:41 -0600

    prima actualización

--- src/TalonarioConsola/TalonarioConsola.java
+++ src/TalonarioConsola/TalonarioConsola.java
@@ -5,7 +5,7 @@ public class TalonarioConsola {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hola");
+		System.out.println("hola...");
 
 	}
 
